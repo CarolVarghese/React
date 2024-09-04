cfdc9ad508b394ae953d4660fcc03f5d7fd19abd
Carol Varghese <carolvarghese23@gmail.com>
2024-09-04 10:47:12 +0530

    Added buttons For user entry

--- BusTicketBooking/src/components/landingPage.tsx
+++ BusTicketBooking/src/components/landingPage.tsx
@@ -3,12 +3,27 @@ import React from 'react';
 const LandingPage: React.FC = () => {
     return (
         <div className='flex flex-col min-h-screen bg-gray-100'>
-            <header className='bg-green-600 text-white text-center py-16'>
-                <h1 className='text-4xl font-bold'>Bus Ticket Booking</h1>
-                <p className='mt-4 text-xl'>Your journey starts here</p>
-                <button className='mt-6 bg-white text-green-600 py-2 px-4 rounded shadow-lg hover:bg-gray-100'>
+            <header className='relative bg-green-600 text-white-200 text-center py-16 bg-cover bg-center' style={{backgroundImage: "url(https://live.staticflickr.com/4155/34326182980_4724ee29f4_b.jpg)"}}>
+                
+                <div className='relative z-10 flex flex-col items-center'>
+                
+                <div className='bg-black bg-opacity-70 p-4 rounded-md inline-block'>
+                    <h1 className=' text-4xl font-bold text-teal-500'>Bus Ticket Booking</h1>
+                    <p className='mt-4 text-xl text-green-600'>Your journey starts here</p>
+                </div>
+                <div className='mt-6 space-x-4'>
+                    <button className='bg-white text-green-600 py-2 px-4 rounded shadow-lg hover:bg-gray-100'>
+                        Sign In
+                    </button>
+                    <button className='bg-white text-green-600 py-2 px-4 rounded shadow-lg hover:bg-gray-100'>
+                        Sign Up
+                    </button>
+                
+                <button className=' bg-gray-300 text-gray-700 py-2 px-4 rounded shadow-lg hover:bg-gray-200'>
                     Book Now
                 </button>
+                </div>
+                </div>
             </header>
             <section className='flex-1 flex flex-col items-center py-12'>
                 <div className='container mx-auto px-4'>
