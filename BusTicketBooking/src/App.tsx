36195ea6e4936652958fc0867be3e461c25137ce
Carol Varghese <carolvarghese23@gmail.com>
2024-09-04 12:01:46 +0530

    Setup Routing in App.tsx

--- BusTicketBooking/src/App.tsx
+++ BusTicketBooking/src/App.tsx
@@ -1,18 +1,23 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import SignUp from './components/SignUp';
 import Counter from './components/counter'
-
 import './App.css'
 import LandingPage from './components/landingPage'
 
 
-function App() {
+const App: React.FC = () => {
   
 
   return (
-    <>
-      <LandingPage/>
-      <Counter/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </Router>
     
   )
 }
